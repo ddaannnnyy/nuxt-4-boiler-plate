1f6f7a330735a0ece270ecd8551146ba2792aeac
ddaannnnyy <32764014+ddaannnnyy@users.noreply.github.com>
2025-02-28 09:35:41 +1000

    added key concepts to index page, added composable example, and routed page examples

---
+++ app/components/example/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component-body">
+        <h2>Component Example</h2>
+        <h3><code>/app/components/example/index.vue</code></h3>
+        <p>The count being passed as a prop is: {{ count }}</p>
+        <button
+        @click="emitTest"
+        >Emit to change the count to 10</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+    interface EmitInterface {
+        newValue: number;
+    }
+
+    const {count} = defineProps<{
+        count: number
+    }>();
+    const emit = defineEmits<{
+        'emit-label': [value: EmitInterface];
+    }>();
+
+    function emitTest() {
+        emit('emit-label', { newValue: 10 });
+    };
+</script>
+
+<style scoped>
+.component-body {
+    background-color: darkgray;
+    padding: 2rem;
+}
+</style>
---
+++ app/composables/sayHello.ts
@@ -0,0 +1,3 @@
+export const sayHello = () => {
+    console.log('hello from the composable at /app/components/sayHello.ts');
+}
